4865ee12407a921402208cbd06c0e62508893c09
rzepa <odwrotnie@gmail.com>
2016-03-01 14:09:26 +0100

    test

--- src/test/scala/rzepaw/NotificationTest.scala
+++ src/test/scala/rzepaw/NotificationTest.scala
@@ -14,9 +14,9 @@ class NotificationTest
     println(s"Response: $response")
   }
 
-//  "Android notifications" should "work" in {
-//    val n = AndroidNotifier("ERCO.Net", "AIzaSyA-ZxHs-QnMRRJphoU0KydlYMzPLR3KqeI", "APA91bG6WB556e96hrNmwalC7fhoZZl4-vjoZ3JCQ-UMPa8FeitrZrF0ewJvic1TQeRDCsuiOsbkVTRU0WeWvaUEcKcb7FyKFrJKTJj8WwLQhHN3Z98HXFfuFrmgR9yKNHpaBO36PMSP")
-//    val response = Await.result(n.notify("Jakaś dłuższa wiadomość"), Duration.Inf)
-//    println(s"Response: $response")
-//  }
+  "Android notifications" should "work" in {
+    val n = AndroidNotifier("ERCO.Net", "AIzaSyA-ZxHs-QnMRRJphoU0KydlYMzPLR3KqeI", "APA91bG6WB556e96hrNmwalC7fhoZZl4-vjoZ3JCQ-UMPa8FeitrZrF0ewJvic1TQeRDCsuiOsbkVTRU0WeWvaUEcKcb7FyKFrJKTJj8WwLQhHN3Z98HXFfuFrmgR9yKNHpaBO36PMSP")
+    val response = Await.result(n.notify("Jakaś dłuższa wiadomość"), Duration.Inf)
+    println(s"Response: $response")
+  }
 }
